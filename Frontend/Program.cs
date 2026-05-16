92c59a696ded37cca99815eb2891048b3c15b389
moosemorse <a.ivanov6174@gmail.com>
2026-05-16 18:29:17 +0100

    feat: update config

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -15,7 +15,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
