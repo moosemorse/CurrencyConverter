83d14fae1c3226040c520f92e8745448168e3f99
moosemorse <a.ivanov6174@gmail.com>
2026-05-17 13:34:45 +0100

    style: add some annotations/notes

--- Frontend/Components/Models/ConverterInput.cs
+++ Frontend/Components/Models/ConverterInput.cs
@@ -1,10 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Frontend.Components.Models;
 
+/* This might not be necessary but for now from tutorial it seems useful
+   - Add attributes for validation
+    - [Required] : this is required
+    - [StringLength(n)] : this is a string with a length constraint of n
+    - [Range(min, max)] : this is a number with a range constraint of min to max
+    - [RegularExpression(pattern)] : this is a string that must match the regex pattern
+    - [EmailAddress] : this is a string that must be a valid email address
+    - [Phone] : this is a string that must be a valid phone number
+    - [Url] : this is a string that must be a valid URL
+    - [Compare(otherProperty)] : this property must match the value of otherProperty
+    - [CustomValidation(typeof(ValidatorClass), "ValidationMethod")] : this property must pass
+*/
 public class ConverterInput
 {
-    public ExchangeRate StartingRate { get; set; }
-    public int Amount { get; set; }
-    public ExchangeRate TargetRate { get; set; }
+    [Required] // blazor makes sure user fills in field
+    public required ExchangeRate StartingRate { get; set; }
+    [Required]
+    public required int Amount { get; set; }
+    [Required]
+    public required ExchangeRate TargetRate { get; set; }
 
     // maybe useful: ArgumentException.ThrowIfNullOrEmpty(string paramName) for validation
     //               ArgumentException.ThrowIfNullOrWhitespace(string paramName) for string validation <-- acc i think this is already handled by blazor component
