e62093b693533e35af27e9e8e48edb39cde2ac9d
moosemorse <a.ivanov6174@gmail.com>
2026-05-18 17:06:39 +0100

    feat: string converter for json payloads

---
+++ Frontend/Converters/StringConverters.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CurrencyConverter.Frontend.Converters;
+
+/* 
+    Not needed in my case but worth knowing about -- from my tutorial. For cases
+    when Json returns a number but we needed a string type for example
+
+    Usage, [JsonConverter(typeof(StringConverter))] <-- any time we try to deserialize a string,
+    it will use this converter to convert it to a string even if it's a number in the JSON
+
+*/
+public class StringConverter : JsonConverter<string?>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return (reader.TokenType == JsonTokenType.Number) ? reader.GetInt32().ToString() : reader.GetString();
+    }
+
+    public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value);
+    }
+}
