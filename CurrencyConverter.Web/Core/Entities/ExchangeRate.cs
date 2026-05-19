f914b22aa1e0badf9470b69a7b261c3f54a6be95
moosemorse <a.ivanov6174@gmail.com>
2026-05-19 13:40:46 +0100

    refactor: currency selector into component

--- CurrencyConverter.Web/Core/Entities/ExchangeRate.cs
+++ CurrencyConverter.Web/Core/Entities/ExchangeRate.cs
@@ -1,6 +1,7 @@
 namespace CurrencyConverter.Web.Core.Entities;
 
-
+// another consideration, we make a class here to represent exchange rates instead of enum
+// would avoid using hardcoded enums and then fetch possible exchange rates from api
 public enum ExchangeRate
 {
     GBP,
--- CurrencyConverter.Web/Infrastructure/Services/PrettyPrintService.cs
+++ CurrencyConverter.Web/Infrastructure/Services/PrettyPrintService.cs
@@ -4,7 +4,6 @@ namespace CurrencyConverter.Web.Infrastructure.Services;
 
 public class PrettyPrintService(ICurrencyNameProvider currencyNameProvider) : IPrettyPrintService
 {
-        // We cache the dictionary so we only fetch it from the internet ONCE per session
     private Dictionary<string, string>? _cachedNames;
 
     public async Task<Dictionary<string, string>> GetAllCurrencyNamesAsync()
@@ -12,7 +11,7 @@ public class PrettyPrintService(ICurrencyNameProvider currencyNameProvider) : IP
         if (_cachedNames != null) return _cachedNames;
         
         _cachedNames = await currencyNameProvider.FetchCurrencyNamesAsync();
-        
+
         return _cachedNames;
     }
 }
